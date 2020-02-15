8bcc5548cc6668760e82f7dcd0a15574c6445923
草梅友仁 <996881204@qq.com>
2020-02-15 20:09:31 +0800

    feat: 新增Date、Buffer对象的转换插件

---
+++ README.md
@@ -0,0 +1,6 @@
+# safe-json-type-plugins
+
+本包为[safe-json-type](https://github.com/CaoMeiYouRen/safe-json-type)的插件，实现了Date、Buffer等对象的转换。
+
+注意browser.ts中只导出了Date类的插件，如需在前端使用Buffer对象，请在模块化工程中使用
+
---
+++ test/index.test.ts
@@ -0,0 +1,20 @@
+import { SafeJsonPluginDate,SafeJsonPluginBuffer } from '../src'
+import 'should'
+describe('SafeJsonPlugin', () => {
+    it('测试Date类的转换', () => {
+        const plugin = new SafeJsonPluginDate()
+        const date = new Date('2019-01-01T00:00:00Z')
+        const obj1 = plugin.serialize(date)
+        obj1.should.deepEqual({ __type: 'Date', iso: '2019-01-01T00:00:00.000Z' });
+        const date2 = plugin.deserialize(obj1);
+        (date2 instanceof Date).should.ok();
+    })
+    it('测试Buffer类的转换', () => {
+        const plugin = new SafeJsonPluginBuffer()
+        const buff = Buffer.from('123456')
+        const obj1 = plugin.serialize(buff)
+        obj1.should.deepEqual({ __type: 'Bytes', base64: 'MTIzNDU2' });
+        const buff2 = plugin.deserialize(obj1);
+        (buff2 instanceof Buffer).should.ok();
+    })
+})
