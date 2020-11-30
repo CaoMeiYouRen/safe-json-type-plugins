fcdb4597893b6bb22d02862358cced892e82f300
CaoMeiYouRen <996881204@qq.com>
2020-11-30 21:53:19 +0800

    build: 修改编译配置

--- rollup.config.js
+++ rollup.config.js
@@ -2,23 +2,39 @@ import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
-const plugins = [
-    nodeResolve({
-        browser: true,
-        preferBuiltins: true,
-    }),
-    commonjs({
-        sourceMap: false,
-    }),
-    typescript({
-        tsconfig: 'tsconfig.build.json',
-    }),
-]
-const pluginsAndTerser = plugins.concat([
-    terser({
-        module: true,
-    }),
-])
+import { dependencies } from './package.json'
+const external = Object.keys(dependencies)
+const env = process.env
+const IS_PROD = env.NODE_ENV === 'production'
+function getPlugins({ isBrowser = false, isMin = false, isDeclaration = false }) {
+    const plugins = []
+    plugins.push(
+        nodeResolve({
+            browser: isBrowser,
+            preferBuiltins: true,
+        }),
+    )
+    plugins.push(
+        typescript({
+            tsconfig: isDeclaration ? 'tsconfig.json' : 'tsconfig.build.json',
+            esModuleInterop: true,
+            allowSyntheticDefaultImports: true,
+        }),
+    )
+    plugins.push(
+        commonjs({
+            sourceMap: false,
+        }),
+    )
+    if (isMin) {
+        plugins.push(
+            terser({
+                module: true,
+            }),
+        )
+    }
+    return plugins
+}
 export default [
     {
         input: 'src/index.ts', // 生成类型文件
@@ -27,38 +43,53 @@ export default [
             format: 'cjs',
             name: 'SafeJsonTypePlugins',
         },
-        plugins: [
-            nodeResolve({
-                browser: true,
-                preferBuiltins: true,
-            }),
-            commonjs({
-                sourceMap: false,
-            }),
-            typescript({
-                tsconfig: 'tsconfig.json',
-            }),
-        ],
+        plugins: getPlugins({
+            isBrowser: false,
+            isDeclaration: true,
+            isMin: false,
+        }),
     },
     {
         input: 'src/index.ts',
+        external,
         output: {
             file: 'dist/index.js',
             format: 'cjs',
             name: 'SafeJsonTypePlugins',
         },
-        plugins,
+        plugins: getPlugins({
+            isBrowser: false,
+            isDeclaration: false,
+            isMin: false,
+        }),
     },
     {
         input: 'src/index.ts',
+        external,
         output: {
             file: 'dist/index.esm.js',
             format: 'esm',
             name: 'SafeJsonTypePlugins',
         },
-        plugins,
+        plugins: getPlugins({
+            isBrowser: false,
+            isDeclaration: false,
+            isMin: false,
+        }),
     },
-
+    {
+        input: 'src/browser.ts',
+        output: {
+            file: 'dist/browser.js',
+            format: 'umd',
+            name: 'SafeJsonTypePlugins',
+        },
+        plugins: getPlugins({
+            isBrowser: true,
+            isDeclaration: false,
+            isMin: false,
+        }),
+    },
     {
         input: 'src/browser.ts',
         output: {
@@ -66,7 +97,11 @@ export default [
             format: 'umd',
             name: 'SafeJsonTypePlugins',
         },
-        plugins: pluginsAndTerser,
+        plugins: getPlugins({
+            isBrowser: true,
+            isDeclaration: false,
+            isMin: IS_PROD,
+        }),
     },
     {
         input: 'src/browser.ts',
@@ -75,6 +110,10 @@ export default [
             format: 'esm',
             name: 'SafeJsonTypePlugins',
         },
-        plugins: pluginsAndTerser,
+        plugins: getPlugins({
+            isBrowser: true,
+            isDeclaration: false,
+            isMin: IS_PROD,
+        }),
     },
 ]
